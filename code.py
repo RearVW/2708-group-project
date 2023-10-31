d1562098114768fc39b08b4ed2792010f7001376
Roger <huang.lezhi@u.nus.edu>
2023-10-31 23:41:33 +0800

    code goes here

--- code.py
+++ code.py
@@ -1,4 +1,13 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.formula.api as smf
 import geopandas
+
+pd.set_option('display.max_columns', 4)
+df_gas = pd.read_csv('data\df_gas.csv')
+#df_gas = df_gas.loc[df_gas['tg_consumption_gwh'] > 5]
+df_gas_pb = df_gas.loc[df_gas['housing_type'] == 'Public Housing']
+df_gas_1_2 = df_gas.loc[df_gas['dwelling_type'] == '1-Room / 2-Room']
+
+print(df_gas_1_2)
